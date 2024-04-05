6cfab6261e8c84336660ce908c8c0ad82f7781af
Lime <agahong1@gmail.com>
2024-04-05 11:58:39 +0900

    First Commit, Frame of Simple Application Sample.

---
+++ app/src/main/java/com/example/hurryup/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.hurryup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceStare) {
+        super.onCreate(savedInstanceStare);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000); // 3초 있다 메인액티비티로
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
