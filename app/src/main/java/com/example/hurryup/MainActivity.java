778eef406bbdb3e79d8af992cbf4364a0ee49319
Lime <agahong1@gmail.com>
2024-04-11 19:25:57 +0900

    Permission & Bluetooth Update (in MainActivity.java) Update minSdkVersion to 31

--- app/src/main/java/com/example/hurryup/MainActivity.java
+++ app/src/main/java/com/example/hurryup/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.hurryup;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.hurryup.support.PermissionSupport;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -13,8 +18,57 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.hurryup.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     private ActivityMainBinding binding;
+
+    private PermissionSupport permission;
+    public BluetoothAdapter mBluetoothAdapter;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //여기서도 리턴이 false로 들어온다면 (사용자가 권한 허용 거부)
+        if (!permission.permissionResult(requestCode, permissions, grantResults)) {
+            // 다시 permission 요청
+            permission.requestPermission();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void permissionCheck() {
+        // PermissionSupport.java 클래스 객체 생성
+        permission = new PermissionSupport(this, this);
+
+        // 권한 체크 후 리턴이 false로 들어오면
+        if (!permission.checkPermission()) {
+            //권한 요청
+            permission.requestPermission();
+        }
+    }
+
+    private void SetBluetooth() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // 장치가 블루투스를 지원하지 않는 경우.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "블루투스 미지원 기기입니다.", Toast.LENGTH_LONG).show();
+
+            // Safe종료
+            moveTaskToBack(true);
+            finish();
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+
+        // 블루투스가 비활성화 상태 (기기에 블루투스가 꺼져있음)
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!permission.checkPermission()) {
+                permission.requestPermission();
+            }
+            else{
+                Intent bIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(bIntent, 0);
+            }
+        }
+
+    }
 
     private void SetNavigation(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -34,7 +88,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        permissionCheck();
+        SetBluetooth();
         SetNavigation();
     }
 
