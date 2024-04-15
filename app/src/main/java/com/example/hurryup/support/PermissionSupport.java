0c85478cfb965989765a75d4f70b2af059acadcb
Lime <agahong1@gmail.com>
2024-04-15 16:52:48 +0900

    Bugfix : Permission requset logic

--- app/src/main/java/com/example/hurryup/support/PermissionSupport.java
+++ app/src/main/java/com/example/hurryup/support/PermissionSupport.java
@@ -3,17 +3,21 @@ package com.example.hurryup.support;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.example.hurryup.MainActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PermissionSupport {
-    private final Context context;
-    private final Activity activity;
+    private Context context;
+    private Activity activity;
 
     //요청할 권한 배열 저장
     private final String[] permissions = {
@@ -26,7 +30,7 @@ public class PermissionSupport {
 
     //권한 요청시 발생하는 창에 대한 결과값을 받기 위해 지정해주는 int 형
     //원하는 임의의 숫자 지정
-    private final int MULTIPLE_PERMISSIONS = 1023; //요청에 대한 결과값 확인을 위해 RequestCode를 final로 정의
+    private final int MULTIPLE_PERMISSIONS = 1001; //요청에 대한 결과값 확인을 위해 RequestCode를 final로 정의
 
     //생성자에서 Activity와 Context를 파라미터로 받아
     public PermissionSupport(Activity _activity, Context _context){
@@ -56,7 +60,7 @@ public class PermissionSupport {
     //요청한 권한에 대한 결과값 판단 및 처리
     public boolean permissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
         //우선 requestCode가 아까 위에 final로 선언하였던 숫자와 맞는지, 결과값의 길이가 0보다는 큰지 먼저 체크
-        if(requestCode == MULTIPLE_PERMISSIONS && (grantResults.length >0)) {
+        if(requestCode == MULTIPLE_PERMISSIONS && (grantResults.length > 0)) {
             for (int grantResult : grantResults) {
                 //grantResults 가 0이면 사용자가 허용한 것 / -1이면 거부한 것
                 //-1이 있는지 체크하여 하나라도 -1이 나온다면 false를 리턴
