60ca5b89300df26e3530d970d914eb583e531982
Lime <agahong1@gmail.com>
2024-04-15 18:55:01 +0900

    PairDevice merge complete

--- app/src/main/java/com/example/hurryup/MainActivity.java
+++ app/src/main/java/com/example/hurryup/MainActivity.java
@@ -21,7 +21,6 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.PermissionChecker;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -41,20 +40,31 @@ public class MainActivity extends AppCompatActivity {
     public BluetoothAdapter mBluetoothAdapter;
 
 
-    // launcher 선언
-    private ActivityResultLauncher<Intent> mStartForResult ;
-
     // 블루투스 페어링
     Set<BluetoothDevice> pairedDevices;
     ArrayAdapter<String> adapter;
+
+
+    // launcher 선언 및 초기화
+    private ActivityResultLauncher<Intent> mStartForResult;
+    private void launcherInitialize(){
+        mStartForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == RESULT_CANCELED){
+                        // 블루투스 켤때까지 물어봄
+                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        mStartForResult.launch(enableBtIntent);
+                    }
+                }
+        );
+    }
 
     public void selectPairedDevice() {
         if (!permission.checkPermission()) {
             permission.requestPermission();
         }
         pairedDevices = mBluetoothAdapter.getBondedDevices();
-
-        Toast.makeText(MainActivity.this,"123",Toast.LENGTH_SHORT);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("장치 선택");
@@ -67,13 +77,13 @@ public class MainActivity extends AppCompatActivity {
 
         // 페어링된 장치중에 "HurryUp"문자열을 포함하는 장치가 있는지 확인.
         // 없다면 블루투스 설정페이지를 열고 프로그램 종료
-        if(!pairedList.stream().anyMatch(element -> element.contains("HurryUp"))){
-            Toast.makeText(this, "최초 사용시 블루투스 연결이 필요합니다.", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
-            startActivity(intent);
-            finish();
-            return;
-        }
+//        if(!pairedList.stream().anyMatch(element -> element.contains("HurryUp"))){
+//            Toast.makeText(this, "최초 사용시 블루투스 연결이 필요합니다.", Toast.LENGTH_SHORT).show();
+//            Intent intent = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
+//            startActivity(intent);
+//            finish();
+//            return;
+//        }
 
         final CharSequence[] devices = pairedList.toArray(new CharSequence[pairedList.size()]);
         builder.setItems(devices, new DialogInterface.OnClickListener() {
@@ -109,10 +119,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         //리턴이 true로 들어온다면 (사용자가 권한 허용)
-        if (permission.permissionResult(requestCode, permissions, grantResults)) {
-            SetBluetooth();
-        }
-        else {
+        if (!permission.permissionResult(requestCode, permissions, grantResults)) {
             // 한번 거부한 권한은 직접 권한을 허용해야 앱 실행 가능하다 알림
             Toast.makeText(getApplicationContext(), "설정->권한에 있는 모든 권한을 허용해주세요.", Toast.LENGTH_LONG).show();
 
@@ -142,21 +149,8 @@ public class MainActivity extends AppCompatActivity {
         if (!mBluetoothAdapter.isEnabled()) {
             // 블루투스를 활성화 하도록 안내
             Intent bIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            mStartForResult = registerForActivityResult(
-                    new ActivityResultContracts.StartActivityForResult(),
-                    result -> {
-                        if(result.getResultCode() == RESULT_CANCELED){
-                            // 블루투스 켤때까지 물어봄
-                            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                            mStartForResult.launch(enableBtIntent);
-                        }
-                    }
-            );
             mStartForResult.launch(bIntent);
         }
-
-
-        selectPairedDevice();
     }
 
     private void SetNavigation(){
@@ -177,7 +171,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        launcherInitialize();
         permissionCheck();
+        SetBluetooth();
+
+        //버튼 기능으로 빼기 or 페어링 확인 스레드 생성
+        selectPairedDevice();
+
+
+
         SetNavigation();
     }
 }
